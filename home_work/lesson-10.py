fedb94f25e1ab7fd0e28c640175e1ab688c5065c
Александр Занин <bsalexandrzanin@gmail.com>
2024-04-29 16:53:12 +0300

    add work files in project

--- home_work/lesson-10.py
+++ home_work/lesson-10.py
@@ -1,1 +1,13 @@
-sdf
+import random
+
+import requests
+
+activity_num = random.randint(1, 5)
+url = "https://www.boredapi.com/api/activity"
+params = {
+    'participants': activity_num,
+}
+
+r = requests.get(url)
+result = r.json()
+print(result['activity'])
